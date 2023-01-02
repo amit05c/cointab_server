859adab7317d746745296d6f00a465eab4057d60
amig05c <amitghosh0595c@gmail.com>
2023-01-02 22:49:47 +0530

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express= require("express")
 const cors= require("cors")
 const { connection } = require("./config/db")
 const { usersRoutes } = require("./routes/user.router")
-
+// db
 const app= express()
 app.use(cors())
 app.use(express.json())
